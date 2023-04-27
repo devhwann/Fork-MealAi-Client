d1223e52cb5a4b4fdc531d24d83852780671d531
jisoo jamie <jisoojamie@gmail.com>
2023-04-27 23:26:32 +0900

    feat: 버튼 컴포넌트 스타일 적용 함수

---
+++ src/utils/GetStyle.ts
@@ -0,0 +1,14 @@
+// BasicButton, TinyButton 스타일 적용 함수
+
+export default function GetStyle(style: string) {
+	switch (style) {
+		case "primary":
+			return "bg-primary-1 text-white";
+		case "bg":
+			return "bg-bg-1 text-primary-1";
+		case "deactivated":
+			return "bg-gray-8 text-gray-4";
+		case "gray":
+			return "bg-gray-1 text-white";
+	}
+}
