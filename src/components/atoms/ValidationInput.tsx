9097eeb4b45df3ab0d4e8077ac4cf102c91d2133
서지영 Jiyeong Seo <bestzenio@gmail.com>
2023-04-27 20:03:13 +0900

    feat : components - inputs

---
+++ src/components/atoms/ValidationInput.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { InputProps } from "./Input";
+
+export interface ValidationInputProps extends InputProps {
+	value: string;
+	isError: boolean;
+	errorMessage: string;
+}
+
+function getValidationColor(isError: boolean, value: string) {
+	if (isError) {
+		return "system-error"; // 유효하지 않은 값
+	}
+	if (value.length === 0) {
+		return "gray-7"; // 빈 값
+	}
+	return "system-success"; // 유효한 값
+}
+
+const ValidationInput = ({
+	type,
+	name,
+	id,
+	value,
+	placeholder,
+	onChange,
+	isError,
+	errorMessage,
+}: ValidationInputProps) => {
+	return (
+		<>
+			<input
+				type={type}
+				name={name}
+				id={id}
+				value={value}
+				placeholder={placeholder}
+				onChange={onChange}
+				className={`input input-bordered w-full border-${getValidationColor(isError, value)}`}
+			/>
+			<p className={`text-sm text-${getValidationColor(isError, value)}`}>{errorMessage}</p>
+		</>
+	);
+};
+
+export default ValidationInput;
---
+++ src/components/molecules/InputWithLabel.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import InputLabel, { InputLabelProps } from "../atoms/InputLabel";
+import ValidationInput, { ValidationInputProps } from "../atoms/ValidationInput";
+
+interface InputWithLabelProps extends ValidationInputProps, InputLabelProps {}
+
+const InputWithLabel = ({
+	type,
+	name,
+	id,
+	value,
+	placeholder,
+	onChange,
+	isError,
+	errorMessage,
+	label,
+	htmlFor,
+}: InputWithLabelProps) => {
+	return (
+		<>
+			<InputLabel label={label} htmlFor={htmlFor} />
+			<ValidationInput
+				type={type}
+				name={name}
+				id={id}
+				value={value}
+				placeholder={placeholder}
+				onChange={onChange}
+				isError={isError}
+				errorMessage={errorMessage}
+			/>
+		</>
+	);
+};
+
+export default InputWithLabel;
