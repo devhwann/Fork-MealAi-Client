5e777816473c628e711ef5269613da34b1794b56
서지영 Jiyeong Seo <bestzenio@gmail.com>
2023-04-28 22:52:23 +0900

    test : mock api

---
+++ src/api/user/index.ts
@@ -0,0 +1,11 @@
+import { api, axios, API_ENDPOINT } from "@/utils/axios.utils";
+
+/**
+ * user apis
+ * 사용자 조회 및 업데이트, 삭제를 담당하는 api 객체
+ */
+export const userApi = Object.freeze({
+	async findUserById(userId: string) {
+		return await api.get(`/api/users?user_id=${userId}`);
+	},
+});
