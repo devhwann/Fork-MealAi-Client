15d6dd1e73105cb02536099f5663a0ac3aab51cc
JiHwan <hg_07@naver.com>
2023-05-08 15:16:20 +0900

    test: SEO

--- src/App.tsx
+++ src/App.tsx
@@ -5,27 +5,32 @@ import Fallback from "@/components/errorrBoundary/CommonErrorBoundary";
 import { HelmetProvider, Helmet } from "react-helmet-async";
 import MetaTag from "./utils/getMetaTag";
 
+const helmetContext = {};
+
 const App = () => {
 	return (
-		<HelmetProvider>
-			{/* <MetaTag
-				title={"MealAi"}
-				description={"식단 관리 서비스"}
-				keywords={"다이어트"}
-				// imgsrc={"@/assets/icon_food_add.svg"}
-				url={"https://github.com"}
-				locale={"ko_KR"}
-			/> */}
-			<Helmet>
-				<meta charSet="utf-8" />
-				<title>mea</title>
-				<meta name="description" content="'고양이"></meta>
+		// <HelmetProvider>
+		// 	<MetaTag
+		// 		title={"MealAi"}
+		// 		description={"식단 관리 서비스"}
+		// 		keywords={"다이어트"}
+		// 		// imgsrc={"@/assets/icon_food_add.svg"}
+		// 		url={"https://github.com"}
+		// 		locale={"ko_KR"}
+		// 	/>
+		<HelmetProvider context={helmetContext}>
+			<Helmet prioritizeSeoTags>
+				<title>Hello World</title>
+				<link rel="canonical" href="https://www.tacobell.com/" />
 			</Helmet>
+			<h1>Hello World</h1>
 			<GlobalStyle />
 			<ErrorBoundary FallbackComponent={Fallback}>
 				<AppRouter />
 			</ErrorBoundary>
 		</HelmetProvider>
+
+		// </HelmetProvider>
 	);
 };
 
--- src/utils/getMetaTag.tsx
+++ src/utils/getMetaTag.tsx
@@ -11,28 +11,28 @@ interface TagContentProps {
 
 const MetaTag = (props: TagContentProps) => {
 	return (
-		// <HelmetProvider>
-		<Helmet>
-			<title>{props.title}</title>
+		<HelmetProvider>
+			<Helmet>
+				<title>{props.title}</title>
 
-			<meta name="description" content={props.description} />
-			<meta name="keywords" content={props.keywords} />
+				<meta name="description" content={props.description} />
+				<meta name="keywords" content={props.keywords} />
 
-			<meta property="og:type" content="website" />
-			<meta property="og:title" content={props.title} />
-			<meta property="og:site_name" content={props.title} />
-			<meta property="og:description" content={props.description} />
-			{/* <meta property="og:image" content={props.imgsrc} /> */}
-			<meta property="og:url" content={props.url} />
-			<meta property="og:locale" content={props.locale} />
+				<meta property="og:type" content="website" />
+				<meta property="og:title" content={props.title} />
+				<meta property="og:site_name" content={props.title} />
+				<meta property="og:description" content={props.description} />
+				{/* <meta property="og:image" content={props.imgsrc} /> */}
+				<meta property="og:url" content={props.url} />
+				<meta property="og:locale" content={props.locale} />
 
-			<meta name="twitter:title" content={props.title} />
-			<meta name="twitter:description" content={props.description} />
-			{/* <meta name="twitter:image" content={props.imgsrc} /> */}
+				<meta name="twitter:title" content={props.title} />
+				<meta name="twitter:description" content={props.description} />
+				{/* <meta name="twitter:image" content={props.imgsrc} /> */}
 
-			<link rel="canonical" href={props.url} />
-		</Helmet>
-		// </HelmetProvider>
+				<link rel="canonical" href={props.url} />
+			</Helmet>
+		</HelmetProvider>
 	);
 };
 
