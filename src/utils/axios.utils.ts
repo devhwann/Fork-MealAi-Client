13e18841d91d9f80341ed7cebdd2aad707c6b762
서지영 Jiyeong Seo <bestzenio@gmail.com>
2023-05-14 02:01:40 +0900

    feat : axios multiple requests 관련 interceptor 수정

--- src/utils/axios.utils.ts
+++ src/utils/axios.utils.ts
@@ -9,7 +9,7 @@ const axiosOptions = {
 	headers: {
 		"Content-Type": "application/json",
 	},
-	timeout: 3000, //  3초 지나면 요청 중단, 임의로 해놨어요
+	timeout: 3000,
 };
 
 const axiosHandler = axios.create(axiosOptions);
@@ -19,6 +19,8 @@ axiosHandler.interceptors.request.use(
 		// 요청을 보내기 전에 수행할 일
 		if (localStorage.getItem("accessToken")) {
 			config.headers["authorization-"] = `Bearer ${localStorage.getItem("accessToken")}`;
+		} else {
+			config.headers["authorization-"] = null;
 		}
 		return config;
 	},
@@ -28,24 +30,40 @@ axiosHandler.interceptors.request.use(
 	}
 );
 
-axiosHandler.interceptors.response.use(
-	function (res) {
-		return res;
-	},
-	async function (error) {
-		const originalConfig = error.config;
-		const code = error.response.data.error_code;
+// * 새 작업
 
-		if (code === 2002 || code === 2004 || code === 2005 || code === 2006) {
+// refresh token multi request 처리
+let isRefreshing = false;
+let refreshSubscribers: any[] = [];
+
+// 대기열
+const processQueue = (err: unknown | null, token: null) => {
+	refreshSubscribers.forEach((callback) => callback(token));
+	refreshSubscribers = [];
+};
+
+// refresh token 관련 오류 시, 스토리지를 비우는 함수
+const handleResetTokens = async () => {
+	if (!isRefreshing) {
+		isRefreshing = true;
+		try {
 			alert("토큰이 만료되어 자동으로 로그아웃 되었습니다. 다시 로그인 해주세요🤗");
 			localStorage.clear();
 			if (window !== undefined) {
 				location.href = "/auth/sign-in";
 			}
-			return;
+			processQueue(null, null);
+		} catch (err) {
+			processQueue(err, null);
 		}
+	}
+};
 
-		if (code === 2001 || code === 2003 || code === 2007) {
+// access token 재발급
+const getRefreshToken = async () => {
+	if (!isRefreshing) {
+		isRefreshing = true;
+		try {
 			const currentRefreshToken = await localStorage.getItem("refreshToken");
 
 			const data = await authApi.authRefreshRequest("/api/auth/refresh", {
@@ -55,12 +73,88 @@ axiosHandler.interceptors.response.use(
 			localStorage.setItem("accessToken", data.data.access_token);
 			localStorage.setItem("refreshToken", data.data.refresh_token);
 			axiosHandler.defaults.headers.common["authorization-"] = `Bearer ${localStorage.getItem("accessToken")}`;
+			processQueue(null, data.data.access_token);
+		} catch (err) {
+			processQueue(err, null);
+		} finally {
+			isRefreshing = false;
+		}
+	}
+};
 
-			return axiosHandler(originalConfig);
+axiosHandler.interceptors.response.use(
+	function (res) {
+		return res;
+	},
+	async function (error) {
+		const originalConfig = error.config;
+		const code = error.response.data.error_code;
+
+		if (code === 2002 || code === 2005 || code === 2006) {
+			try {
+				await handleResetTokens();
+				return;
+			} catch (err) {
+				return Promise.reject(error);
+			}
+		}
+
+		if (code === 2001 || code === 2003 || code === 2004 || code === 2007) {
+			try {
+				await getRefreshToken();
+				return await axiosHandler(originalConfig);
+			} catch (err) {
+				return Promise.reject(error);
+			}
 		}
 
 		return Promise.reject(error);
 	}
 );
+
+// * 기존 interceptor
+
+// axiosHandler.interceptors.response.use(
+// 	function (res) {
+// 		return res;
+// 	},
+// 	async function (error) {
+// 		const originalConfig = error.config;
+// 		const code = error.response.data.error_code;
+
+// 		console.log("error code!!", code);
+
+// 		// ! 2003 : 삭제해도 감지가 안되고 데이터가 받아와짐
+
+// 		if (code === 2002 || code === 2003 || code === 2005 || code === 2006) {
+// 			alert("토큰이 만료되어 자동으로 로그아웃 되었습니다. 다시 로그인 해주세요🤗");
+// 			localStorage.clear();
+// 			if (window !== undefined) {
+// 				location.href = "/auth/sign-in";
+// 			}
+// 			return;
+// 		}
+
+// 		// ! 2004 : accessToken 변질됐을 때, 페이지이동시 refresh 잘됨 / 새로고침하면 안됨....
+
+// 		if (code === 2001 || code === 2004 || code === 2007) {
+// 			// console.log("2001, 2004, 2007 error가 몇 번 뜰까욤");
+
+// 			const currentRefreshToken = localStorage.getItem("refreshToken");
+
+// 			const data = await authApi.authRefreshRequest("/api/auth/refresh", {
+// 				refresh_token: currentRefreshToken!,
+// 			});
+
+// 			localStorage.setItem("accessToken", data.data.access_token);
+// 			localStorage.setItem("refreshToken", data.data.refresh_token);
+// 			axiosHandler.defaults.headers.common["authorization-"] = `Bearer ${localStorage.getItem("accessToken")}`;
+
+// 			return axiosHandler(originalConfig);
+// 		}
+
+// 		return Promise.reject(error);
+// 	}
+// );
 
 export { axiosHandler, axios, API_ENDPOINT };
